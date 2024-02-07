23a5b5aa749255fe6a0f82a5f5de420dde756190
Noah Schütz <noah.schuetz@ipk.fraunhofer.de>
2024-02-07 15:12:38 +0100

    evaluations prototype

--- cloudnerf-frontend/src/views/pages/Evaluations.js
+++ cloudnerf-frontend/src/views/pages/Evaluations.js
@@ -1,6 +1,20 @@
+import React from 'react';
+// import TwoScenes from './ThreeScene';
+import {Row, Col} from 'antd';
+import ThreeScene from './ThreeScene';
+
 function Evaluations() {
+
     return (
-        <h1>Evaluations</h1>
+        <div>
+            <h1>Evaluations</h1>
+            <p>Here is where the evaluations will be displayed</p>
+
+            <Row>
+                <ThreeScene />
+            </Row>           
+
+        </div>        
     );
 }
 
