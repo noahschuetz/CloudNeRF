3388719814d562098aa43e3053152617ed435687
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-08 18:24:55 +0000

    use env keys in supabase client

--- cloudnerf-backend/supabaseClient.js
+++ cloudnerf-backend/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import { config } from "dotenv";
+config()
 
 
-const supabaseUrl = "http://127.0.0.1:54321";
-// const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
-// const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+console.log("SUPABASE URL ", supabaseUrl, "SUPABASE KEY",supabaseKey)
 export const supabase = createClient(supabaseUrl, supabaseKey);
