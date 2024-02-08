3fbfe3168508b30da9128ade604d821b83e744ee
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-08 11:17:29 +0100

    add neus and nerfacto

--- cloudnerf-backend/run_models/configs.js
+++ cloudnerf-backend/run_models/configs.js
@@ -17,4 +17,40 @@ export const runModelsConfigs = [
             "/workspace/data"
 		],
 	},
+	{
+		modelId: "neus",
+		cmd: "docker",
+		cmdArgs: [
+			"run",
+			"--gpus",
+			"all",
+			"-v",
+			`${process.env.ROOT_DIR}/tmp/neus/data/:/workspace/data/`,
+            "--rm",
+            "--shm-size=12gb",
+            "dromni/nerfstudio:1.0.0",
+            "ns-train",
+            "neus",
+            "--data",
+            "/workspace/data"
+		],
+	},
+	{
+		modelId: "nerfacto",
+		cmd: "docker",
+		cmdArgs: [
+			"run",
+			"--gpus",
+			"all",
+			"-v",
+			`${process.env.ROOT_DIR}/tmp/nerfacto/data/:/workspace/data/`,
+            "--rm",
+            "--shm-size=12gb",
+            "dromni/nerfstudio:1.0.0",
+            "ns-train",
+            "nerfacto",
+            "--data",
+            "/workspace/data"
+		],
+	},
 ];
