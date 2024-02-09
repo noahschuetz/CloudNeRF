d97abdbc51dfc211542d34dd458830976b5c6548
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-09 15:50:46 +0000

    function for installing models

--- cloudnerf-backend/run_models/utils.js
+++ cloudnerf-backend/run_models/utils.js
@@ -1,4 +1,4 @@
-import { mkdirSync, writeFileSync } from "fs";
+import { chmodSync, mkdirSync, writeFileSync } from "fs";
 import path from "path";
 import { supabase } from "../supabaseClient.js";
 import { spawnSync } from "child_process";
@@ -8,19 +8,33 @@ export async function loadDatasetIntoTemporaryDirectory(modelId, datasetId) {
 	const tmpDir = tmpDirForModelRun(modelId);
 	mkdirSync(tmpDir, {
 		recursive: true,
+		mode: 0o777,
 	});
+	chmodSync(tmpDir, 0o777);
 
 	console.log("Reading contents of dataset...");
 	const { data: datasetContents } = await supabase.storage
 		.from("datasets")
 		.list(datasetId);
 
+	// images download
 	const assetDir = datasetContents.filter((d) => d.id === null)[0].name;
 	mkdirSync(path.join(tmpDir, assetDir), { recursive: true });
 
 	const { data: assetDirContents } = await supabase.storage
 		.from("datasets")
-		.list(`${datasetId}/${assetDir}`);
+		.list(`${datasetId}/${assetDir}`, { limit: 1000 });
+
+	console.log("Downloading assets...");
+	for (const assetFile of assetDirContents) {
+		const filename = assetFile.name;
+		const { data } = await supabase.storage
+			.from("datasets")
+			.download(`${datasetId}/${assetDir}/${filename}`);
+		const buffer = Buffer.from(await data.arrayBuffer());
+		writeFileSync(path.join(tmpDir, "data", assetDir, filename), buffer);
+		console.log(`Got ${filename}`);
+	}
 
 	// transforms file download
 	console.log("Downloading transforms file...");
@@ -29,24 +43,12 @@ export async function loadDatasetIntoTemporaryDirectory(modelId, datasetId) {
 		.download(`${datasetId}/transforms.json`);
 
 	const buffer = Buffer.from(await data.arrayBuffer());
-	writeFileSync(path.join(tmpDir, "transforms.json"), buffer);
-
-	// assets donwload
-	console.log("Downloading assets...");
-	for (const assetFile of assetDirContents) {
-		const filename = assetFile.name;
-		const { data } = await supabase.storage
-			.from("datasets")
-			.download(`${datasetId}/${assetDir}/${filename}`);
-		const buffer = Buffer.from(await data.arrayBuffer());
-		writeFileSync(path.join(tmpDir, assetDir, filename), buffer);
-		console.log(`Got ${filename}`);
-	}
+	writeFileSync(path.join(`${tmpDir}/data`, "transforms.json"), buffer);
 }
 
 export function runModel(config) {
 	console.log("Starting training process");
-	const trainingProcess = spawnSync(config.cmd, config.cmdArgs, {
+	const trainingProcess = spawnSync(config.runCmd, config.runCmdArgs, {
 		shell: true, // for windows
 	});
 	console.log(
@@ -56,6 +58,18 @@ export function runModel(config) {
 	);
 }
 
+export function installModel(config){
+	console.log("Installing docker image for modell...");
+	const installProcess = spawnSync(config.installCmd, config.installCmdArgs, {
+		shell: true, // for windows
+	});
+	console.log(
+		"Download finished",
+		`\nSTDERR: ${installProcess.stderr}`,
+		`\nSTDOUT: ${installProcess.stdout}`,
+	);
+}
+
 function tmpDirForModelRun(modelId) {
-	return path.join(process.env.ROOT_DIR, "tmp", modelId, "data");
+	return path.join(process.env.ROOT_DIR, "tmp", modelId);
 }
