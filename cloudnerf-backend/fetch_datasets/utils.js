56fda8181cb43e7f0016b5f81392cc28a1a22467
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-09 15:47:38 +0000

    fix transforms if no file suffix is provided

--- cloudnerf-backend/fetch_datasets/utils.js
+++ cloudnerf-backend/fetch_datasets/utils.js
@@ -9,14 +9,14 @@ export function downloadDataset(config) {
 	console.log("Creating temporary directory for dataset download");
 	const tmpDir = tmpDirForDatasetFetching(config);
 	mkdirSync(tmpDir, { recursive: true });
-	chmodSync(tmpDir, 0o777)
+	chmodSync(tmpDir, 0o777);
 	console.log("Directory path:", tmpDir);
 
 	console.log(
 		`Starting download (cmd: ${config.cmd}, args: ${config.cmdArgs})`,
 	);
 	const downloadProcess = spawnSync(config.cmd, config.cmdArgs, {
-		shell: true // windows
+		shell: true, // windows
 	});
 	console.log(
 		"Download finished",
@@ -28,12 +28,10 @@ export function downloadDataset(config) {
 
 export async function uploadDatasetsToSupabase(config) {
 	console.log("Creating bucket");
-	const bucket = await supabase.storage.createBucket("datasets", {
-		public: true,
-	});
+	const bucket = await supabase.storage.createBucket("datasets");
 	console.log("Bucket:", bucket);
-	if (bucket.error){
-		console.log(bucket.error.message)
+	if (bucket.error) {
+		console.log(bucket.error.message);
 	}
 
 	for (const dsPath of config.datasetPaths) {
@@ -42,14 +40,15 @@ export async function uploadDatasetsToSupabase(config) {
 			tmpDirForDatasetFetching(config),
 			dsPath[2],
 		);
-		const bucketLocation = dsPath[0];
-		const bucketDataSubdir = dsPath[1].split("/").at(-1);
+		const datasetId = dsPath[0];
+
 		console.log("Uploading images");
+
 		for (const filename of readdirSync(dataDir)) {
 			const content = readFileSync(path.join(dataDir, filename));
 			await supabase.storage
 				.from("datasets")
-				.upload(`${bucketLocation}/${bucketDataSubdir}/${filename}`, content)
+				.upload(`${datasetId}/images/${filename}`, content)
 				.then((data, error) => {
 					if (error) {
 						console.log("error", error);
@@ -58,10 +57,29 @@ export async function uploadDatasetsToSupabase(config) {
 					console.log("supabase response", data);
 				});
 		}
-		const transformsContent = readFileSync(transformsDir);
+
+		let transformsJson = readFileSync(transformsDir).toString("utf-8");
+		transformsJson = fixTransformsJson(transformsJson);
 		await supabase.storage
 			.from("datasets")
-			.upload(`${bucketLocation}/transforms.json`, transformsContent)
+			.upload(`${datasetId}/transforms.json`, transformsJson)
+			.then((data, error) => {
+				if (error) {
+					console.log("error", error);
+					res.status(500).send("error");
+				}
+				console.log("supabase response", data);
+			});
+
+		const infoJson = JSON.stringify({
+			name: datasetId,
+			description: `Dataset ${datasetId} from ${config.fetchId} bundle`,
+			images: readdirSync(dataDir).length,
+			compatible_models: ["nerfacto", "neus", "instantngp"],
+		});
+		await supabase.storage
+			.from("datasets")
+			.upload(`${datasetId}/info.json`, infoJson)
 			.then((data, error) => {
 				if (error) {
 					console.log("error", error);
@@ -70,6 +88,27 @@ export async function uploadDatasetsToSupabase(config) {
 				console.log("supabase response", data);
 			});
 	}
+}
+
+export function fixTransformsJson(transformsJson) {
+	const transforms = JSON.parse(transformsJson);
+	for (const frame of transforms.frames) {
+		if (!frame.file_path.includes(".png")) {
+			frame.file_path = `${frame.file_path}.png`;
+		}
+		if (frame.file_path.split("/")[0] === ".") {
+			frame.file_path = frame.file_path.substring(2);
+		}
+		if (frame.file_path.split("/").length > 2) {
+			throw Error(
+				`expected images to be only one dir deeper than transforms.json! File path is ${frame.file_path}`,
+			);
+		}
+		if (frame.file_path.split("/")[0] !== "images") {
+			frame.file_path = `images/${frame.file_path.split("/").at(-1)}`;
+		}
+	}
+	return JSON.stringify(transforms);
 }
 
 export function cleanUp(config) {
