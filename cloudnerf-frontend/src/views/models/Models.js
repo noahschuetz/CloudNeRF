82c4e09b59b01e220d4562085d051d0f733aba1f
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-09 15:51:01 +0000

    use model config in models page

--- cloudnerf-frontend/src/views/models/Models.js
+++ cloudnerf-frontend/src/views/models/Models.js
@@ -1,36 +1,30 @@
 import { Button, Spin, Row, Col, List, Select } from "antd";
-import { useGetDatasetsQuery } from "../../redux/api";
+import {
+	useGetDatasetsQuery,
+	useGetDockerImagesQuery,
+	useGetRunModelConfigsQuery,
+} from "../../redux/api";
 import { useEffect, useState } from "react";
 
 export default function Models() {
 	const { data } = useGetDatasetsQuery();
+	const { data: dockerImages } = useGetDockerImagesQuery();
 	const [selectedDataset, setSelectedDataset] = useState(undefined);
 	const [selectedModel, setSelectedModel] = useState(undefined);
 
-	const models = [
-		{
-			name: "instantngp",
-			label: "Instant-NGP",
-			value: "instantngp",
-			description: "Instant-NGP",
-		},
-		{
-			name: "nerfacto",
-			label: "Nerfacto",
-			value: "nerfacto",
-			description: "Nerfacto",
-		},
-		{
-			name: "neus",
-			label: "Neus",
-			value: "neus",
-			description: "Neus",
-		},
-	];
+	const { data: modelConfigs } = useGetRunModelConfigsQuery();
+
+	const models = modelConfigs.map((mc) => ({
+		...mc,
+		name: mc.modelId,
+		label: mc.displayName,
+		value: mc.modelId,
+		description: mc.displayName,
+	}));
 
 	const handleRunModel = async () => {
 		const res = await fetch(
-			`http://localhost:5000/run_model/${selectedModel}/${selectedDataset}`,
+			`http://localhost:5000/models/run/${selectedModel}/${selectedDataset}`,
 		);
 		console.log(res);
 	};
@@ -41,54 +35,58 @@ export default function Models() {
 				<Col span={24}>
 					<h1>Models</h1>
 
-					<List bordered dataSource={models} renderItem={(item, index) => (
-						<List.Item
-							actions={[
-								<Button>Install</Button>,
-								// <RunModelButton modelId={item.name} datasetId={datasetId} />					
-							]}
-						>
-							<List.Item.Meta
-								title={item.name}
-								description={item.description}
-							/>
-						</List.Item>
-					)}
+					<List
+						bordered
+						dataSource={models}
+						renderItem={(item, index) => (
+							<List.Item
+								actions={[
+									dockerImages ? (dockerImages.include(item.dockerImage) ? (
+										<p>already installed, cool</p>
+									) : (
+										<Button>Install</Button>
+									)) : <Spin />,
+									// <RunModelButton modelId={item.name} datasetId={datasetId} />
+								]}
+							>
+								<List.Item.Meta
+									title={item.name}
+									description={item.description}
+								/>
+							</List.Item>
+						)}
 					/>
 				</Col>
-
 			</Row>
 
 			<Row>
-
 				<Col span={24}>
 					<h1>Run Models</h1>
-
 					Dataset:
-					<Select 
+					<Select
 						placeholder="Select a dataset"
-						style={{ width: 120, marginLeft: 10, marginRight: 10}} 
+						style={{ width: 120, marginLeft: 10, marginRight: 10 }}
 						onChange={(value) => setSelectedDataset(value)}
 						options={data}
 					/>
-
-					Model: 
+					Model:
 					<Select
 						placeholder="Select a model"
-						style={{ width: 120, marginLeft: 10, marginRight: 10}}
+						style={{ width: 120, marginLeft: 10, marginRight: 10 }}
 						onChange={(value) => setSelectedModel(value)}
 						options={models}
 					/>
-
-					<Button 
-						style={{ width: 120, marginLeft: 10}}
-						type="primary" 
-						onClick={() => console.log("Run model")}
-						disabled={selectedDataset === undefined || selectedModel === undefined}
-					>Run</Button>
-					
-
-			{/* <div>
+					<Button
+						style={{ width: 120, marginLeft: 10 }}
+						type="primary"
+						onClick={() => handleRunModel()}
+						disabled={
+							selectedDataset === undefined || selectedModel === undefined
+						}
+					>
+						Run
+					</Button>
+					{/* <div>
 				<RunModelButton modelId="instantngp" datasetId={datasetId}>
 					Instant-NGP
 				</RunModelButton>
@@ -99,8 +97,7 @@ export default function Models() {
 					Neus
 				</RunModelButton>
 			</div> */}
-
-			{/* <div style={{ flex: 1 }}>
+					{/* <div style={{ flex: 1 }}>
 				Selected dataset: {datasetId}
 				{data ? (
 					data.map((d) => (
@@ -114,11 +111,8 @@ export default function Models() {
 					<Spin />
 				)}
 			</div> */}
-
 				</Col>
-
 			</Row>
-
 		</>
 	);
 }
