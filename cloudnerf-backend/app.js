7bcf6abb1d5aa6c96bd1549e57a160cfd0920711
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-02-09 15:50:07 +0000

    formatting and console deletes

--- cloudnerf-backend/app.js
+++ cloudnerf-backend/app.js
@@ -16,10 +16,12 @@ import {
 } from "./fetch_datasets/utils.js";
 import { fetchDatasetConfigs } from "./fetch_datasets/configs.js";
 import {
+	installModel,
 	loadDatasetIntoTemporaryDirectory,
 	runModel,
 } from "./run_models/utils.js";
 import { runModelsConfigs } from "./run_models/configs.js";
+import { spawnSync } from "child_process";
 
 config({ debug: true });
 
@@ -38,15 +40,13 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
 app.get("/datasets", async (req, res) => {
-
 	try {
-		const { data, error } = await supabase.storage.from("datasets").list()
-		console.log("data", data);
+		const { data, error } = await supabase.storage.from("datasets").list();
 
 		if (error) {
 			console.log("error", error);
@@ -56,11 +56,15 @@ app.get("/datasets", async (req, res) => {
 		const allInfo = [];
 
 		for (const dataset of data) {
-			console.log("dataset.name", dataset.name);
-			const { data: fileInfo, error: fileError } = await supabase.storage.from('datasets').download(`${dataset.name}/info.json`);
+			const { data: fileInfo, error: fileError } = await supabase.storage
+				.from("datasets")
+				.download(`${dataset.name}/info.json`);
 
 			if (fileError) {
-				console.error(`Error downloading info.json from folder ${folder.name}:`, fileError);
+				console.error(
+					`Error downloading info.json from folder ${folder.name}:`,
+					fileError,
+				);
 				continue;
 			}
 
@@ -72,16 +76,14 @@ app.get("/datasets", async (req, res) => {
 
 			// Push info data into the array
 			allInfo.push(infoData);
-
 		}
 
 		// Send the combined info data as a response
 		res.json(allInfo);
-
 	} catch (error) {
-		console.error('Error downloading info from folders:', error.message);
-    	res.status(500).json({ error: 'Internal server error' });
-  	}
+		console.error("Error downloading info from folders:", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -124,7 +126,6 @@ app.get("/datasets/:id", (req, res) => {
 
 app.delete("/datasets/:id", async (req, res) => {
 	try {
-		console.log('hier');
 		const { id } = req.params;
 
 		const { data: images, error: imagesError } = await supabase.storage
@@ -168,13 +169,11 @@ app.delete("/datasets/:id", async (req, res) => {
 			res.status(500).send("error");
 		}
 
-		res.send('datasets deleted successfully');
-	
+		res.send("datasets deleted successfully");
 	} catch (error) {
-		console.error('Error deleting dataset:', error.message);
-		res.status(500).json({ error: 'Internal server error' });
-  	}
-
+		console.error("Error deleting dataset:", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 });
 
 app.get("/datasets/:id/images", (req, res) => {
@@ -197,91 +196,96 @@ app.get("/datasets/:id/images", (req, res) => {
 		});
 });
 
-app.post("/datasets/:id/images", upload.array("file", 400), async (req, res) => {
-	const { id } = req.params;
-	console.log("id", id);
-	console.log("files", req.files);
+app.post(
+	"/datasets/:id/images",
+	upload.array("file", 400),
+	async (req, res) => {
+		const { id } = req.params;
+		console.log("id", id);
+		console.log("files", req.files);
 
-	// now upload to supabase
-	const filelocation = `${id}/images/${req.files[0].originalname}`;
+		// now upload to supabase
+		const filelocation = `${id}/images/${req.files[0].originalname}`;
 
-	console.log("filelocation", filelocation);
-	const fileContent = readFileSync(req.files[0].path);
+		console.log("filelocation", filelocation);
+		const fileContent = readFileSync(req.files[0].path);
 
-	try {
-		const { data, error } = await supabase.storage
-			.from("datasets")
-			.upload(filelocation, fileContent, { contentType: req.files[0].mimetype });
+		try {
+			const { data, error } = await supabase.storage
+				.from("datasets")
+				.upload(filelocation, fileContent, {
+					contentType: req.files[0].mimetype,
+				});
 
-		if (error) {
+			if (error) {
+				console.log("error", error);
+				res.status(500).send("error");
+			}
+			console.log("datasets", data);
+
+			// // get how many images are in the folder
+			// const filelocationImages = `${id}/images`;
+			// const { data: images, error: imagesError } = await supabase.storage
+			// 	.from("datasets")
+			// 	.list(filelocationImages);
+
+			// if (imagesError) {
+			// 	console.log("error", imagesError);
+			// 	res.status(500).send("error");
+			// }
+			// console.log("images", images);
+
+			// // update the info.json file with the new about of images
+			// const { data: fileInfo, error: fileError } = await supabase.storage.from('datasets').download(`${id}/info.json`);
+
+			// if (fileError) {
+			// 	console.error(`Error downloading info.json from folder ${id}:`, fileError);
+			// }
+
+			// // Convert Blob to string
+			// const fileInfoText = await fileInfo.text();
+
+			// // Parse the JSON data
+			// const infoData = JSON.parse(fileInfoText);
+
+			// // Update the images count
+			// infoData.images = images.length;
+
+			// // Update the updated info.json file
+			// const { data: updatedInfo, error: updatedInfoError } = supabase.storage
+			// 	.from("datasets")
+			// 	.update(`${id}/info.json`, JSON.stringify(infoData), {
+			// 		contentType: "application/json",
+			// 	});
+
+			// if (updatedInfoError) {
+			// 	console.log("error", updatedInfoError);
+			// 	res.status(500).send("error");
+			// }
+
+			res.send(data);
+
+			// Clean up uploaded file from the server
+			unlink(req.files[0].path, (err) => {
+				if (err) {
+					console.error("Error cleaning up file:", err);
+				} else {
+					console.log("File deleted successfully:", req.files[0].path);
+				}
+			});
+		} catch (error) {
 			console.log("error", error);
 			res.status(500).send("error");
 		}
-		console.log("datasets", data);
-
-		// // get how many images are in the folder
-		// const filelocationImages = `${id}/images`;
-		// const { data: images, error: imagesError } = await supabase.storage
-		// 	.from("datasets")
-		// 	.list(filelocationImages);
-			
-		// if (imagesError) {
-		// 	console.log("error", imagesError);
-		// 	res.status(500).send("error");
-		// }
-		// console.log("images", images);
-
-		// // update the info.json file with the new about of images
-		// const { data: fileInfo, error: fileError } = await supabase.storage.from('datasets').download(`${id}/info.json`);
-
-		// if (fileError) {
-		// 	console.error(`Error downloading info.json from folder ${id}:`, fileError);
-		// }
-
-		// // Convert Blob to string
-		// const fileInfoText = await fileInfo.text();
-
-		// // Parse the JSON data
-		// const infoData = JSON.parse(fileInfoText);
-
-		// // Update the images count
-		// infoData.images = images.length;
-
-		// // Update the updated info.json file
-		// const { data: updatedInfo, error: updatedInfoError } = supabase.storage
-		// 	.from("datasets")
-		// 	.update(`${id}/info.json`, JSON.stringify(infoData), {
-		// 		contentType: "application/json",
-		// 	});
-
-		// if (updatedInfoError) {
-		// 	console.log("error", updatedInfoError);
-		// 	res.status(500).send("error");
-		// }
-
-		res.send(data);
-		
-		// Clean up uploaded file from the server
-		unlink(req.files[0].path, (err) => {
-			if (err) {
-				console.error("Error cleaning up file:", err);
-			} else {
-				console.log("File deleted successfully:", req.files[0].path);
-			}
-		});
-	} catch (error) {
-		console.log("error", error);
-		res.status(500).send("error");
-	}
-
-});
+	},
+);
 
 app.patch("/datasets/:id/images", async (req, res) => {
 	const { id } = req.params;
 	const { data: images, error: imagesError } = await supabase.storage
 		.from("datasets")
 		.list(`${id}/images`);
-		
+
 	if (imagesError) {
 		console.log("error", imagesError);
 		res.status(500).send("error");
@@ -289,7 +293,9 @@ app.patch("/datasets/:id/images", async (req, res) => {
 	console.log("images", images);
 
 	// update the info.json file with the new about of images
-	const { data: fileInfo, error: fileError } = await supabase.storage.from('datasets').download(`${id}/info.json`);
+	const { data: fileInfo, error: fileError } = await supabase.storage
+		.from("datasets")
+		.download(`${id}/info.json`);
 
 	if (fileError) {
 		console.error(`Error downloading info.json from folder ${id}:`, fileError);
@@ -319,7 +325,10 @@ app.patch("/datasets/:id/images", async (req, res) => {
 	res.send(updatedInfo);
 });
 
-app.post("/datasets/:id/transforms", upload.array("file", 400),	(req, res, next) => {
+app.post(
+	"/datasets/:id/transforms",
+	upload.array("file", 400),
+	(req, res, next) => {
 		const { id } = req.params;
 
 		console.log("files", req.files);
